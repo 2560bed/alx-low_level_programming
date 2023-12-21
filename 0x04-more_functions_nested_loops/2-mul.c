950ea233a9e6dc312a30d2ef7f4d5bc53aaf66d6
root <2560bed>
2023-12-21 05:00:23 -0800

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,14 +1,12 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
+*mul - function that multipliestwo numbers
+*@a: first number
+*@b: second number
+*Return: returns result
+*/
+
+int mul(int a, int b)
 {
-  printf("%d\n", mul(98, 1024));
-  printf("%d\n", mul(-402, 4096));
-  return (0);
+return (a * b);
 }
