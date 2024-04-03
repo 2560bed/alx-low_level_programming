b476ee8cd112fd12e56ca44d702c7794cf3255f4
root <2560bed>
2024-04-03 02:50:48 -0700

    ff

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,19 +1,19 @@
-#include "main.h"
+#include "holberton.h"
+
 /**
-*print_most_numbers - prints numbers except 2 and 4
-*
-*Return: returns nothing
-*/
-void print_most_numbers(void)
+ * print_most_numbers - prints the numbers from 0 to 9
+ * (without 2 and 4) with a newline
+ */
+
+void	print_most_numbers(void)
 {
-int number;
-for (number = 48; number < 58; number++)
-{
-if ((number == 50) || (number == 52))
-{
-continue;
-}
-_putchar(number);
-}
-_putchar(10);
+	char	c = '0';
+
+	while (c <= '9')
+	{
+		if (c != '2' && c != '4')
+			_putchar(c);
+		c++;
+	}
+	_putchar('\n');
 }
